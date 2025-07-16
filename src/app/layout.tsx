5b084c7bc948cd233eed9297f019fbe46d0a6469
f00tydystroyer <paletasog@gmail.com>
2025-07-16 18:20:26 +0000

    lol

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,7 +10,7 @@ TODO:  Change these things along with:
   - favicon.ico in /public
  */
 const font = Poppins({ weight: '700', subsets: ['latin']})
-const title = 'Evaristo/'s Site';
+const title = 'Evaristo';
 const description = 'This my personal site';
 const links = [
   {title: 'Pokemon', href: '/pokemon'},
